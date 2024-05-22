50a257d0a5715b860d505fa7f88fe4956e1b5375
Kociarz <dbednorz@tuta.io>
2024-05-22 12:16:40 +0200

    Added displaying .mp3 files feature.

--- Music Player/Music Player/MainWindow.xaml.cs
+++ Music Player/Music Player/MainWindow.xaml.cs
@@ -7,7 +7,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using System.IO;
+using System.Collections.Generic;
 using System.Windows.Shapes;
+using System.Linq;
+using Path = System.IO.Path;
 
 namespace Music_Player;
 
@@ -19,6 +23,27 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        LoadMP3Files();
+    }
+    
+    private void LoadMP3Files()
+    {
+        // Specify the folder path where your .mp3 files are located
+        string folderPath = @"C:\";
+
+        // Check if the folder exists
+        if (Directory.Exists(folderPath))
+        {
+            // Get all .mp3 files in the folder
+            string[] mp3Files = Directory.GetFiles(folderPath, "*.mp3");
+
+            // Display the .mp3 files in a ListBox
+            ListBoxMP3.ItemsSource = mp3Files.Select(Path.GetFileName);
+        }
+        else
+        {
+            MessageBox.Show("Folder does not exist.");
+        }
     }
     
     private void PlayButton_Click(object sender, RoutedEventArgs e)
